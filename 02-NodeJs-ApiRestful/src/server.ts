80fe64efa33376f176544cc7a1abbab088b1b6a8
DanielRPTI <drjunior988@hotmail.com>
2023-11-13 14:41:21 -0300

    feat: Fazendo nosso primeiro insert na table

--- 02-NodeJs-ApiRestful/src/server.ts
+++ 02-NodeJs-ApiRestful/src/server.ts
@@ -1,12 +1,13 @@
 import fastify from 'fastify'
 import { knex } from './database'
-
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .where('amount', 10000)
+    .select('*')
 
-  return tables
+  return transaction
 })
 
 app
