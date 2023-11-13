58f6c9d682c1830e5f3f6753ec6cb516f0db4b40
DanielRPTI <drjunior988@hotmail.com>
2023-11-13 15:13:53 -0300

    feat: usando a dependencia Zod para tratamento de erros das variaveis na dotEnv

--- 02-NodeJs-ApiRestful/src/server.ts
+++ 02-NodeJs-ApiRestful/src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './index'
 const app = fastify()
 
 app.get('/hello', async () => {
@@ -12,7 +13,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Http Server Running!')
