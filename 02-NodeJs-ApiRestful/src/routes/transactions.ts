26ab1b172b53e83b6ddef2f5ae726fc8d6f4664c
DanielRPTI <drjunior988@hotmail.com>
2023-11-14 14:22:37 -0300

    Utilizando Cookies para identificação de users sem um login previo

--- 02-NodeJs-ApiRestful/src/routes/transactions.ts
+++ 02-NodeJs-ApiRestful/src/routes/transactions.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 import { randomUUID } from 'crypto'
 import { knex } from '../database'
 
+// Cookies <-> Formas da gente manter contexto entre reqs
+
 export async function transactionRoutes(app: FastifyInstance) {
   app.get('/', async () => {
     const transactions = await knex('transactions').select()
@@ -37,13 +39,26 @@ export async function transactionRoutes(app: FastifyInstance) {
       amount: z.number(),
       type: z.enum(['credit', 'debit']),
     })
+
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body,
     )
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // seven days
+      })
+    }
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
