aea24d39f6dd08005da507a011be1ce2fd7ecce6
DanielRPTI <drjunior988@hotmail.com>
2023-11-17 17:03:33 -0300

    iniciando a criação dos  proximos testes de listagem por id

--- 02-NodeJs-ApiRestful/test/transactions.test.ts
+++ 02-NodeJs-ApiRestful/test/transactions.test.ts
@@ -1,8 +1,7 @@
-import { it, beforeAll, afterAll, describe, expect } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
-import { randomUUID } from 'crypto'
-
+import { execSync } from 'node:child_process'
 describe('Transactions routes', () => {
   beforeAll(async () => {
     await app.ready()
@@ -11,6 +10,12 @@ describe('Transactions routes', () => {
   afterAll(async () => {
     await app.close()
   })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
   it('should be able to creat a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
@@ -22,7 +27,7 @@ describe('Transactions routes', () => {
       .expect(201)
   })
 
-  it.only('Should be able to list all transactions', async () => {
+  it('Should be able to list all transactions', async () => {
     const createTransactionResponse = await request(app.server)
       .post('/transactions')
       .send({
@@ -45,5 +50,36 @@ describe('Transactions routes', () => {
         amount: 5000,
       }),
     ])
+  })
+
+  it.only('Should be able to list a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+    // verifica se está recebendo um array com objetos
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    )
   })
 })
