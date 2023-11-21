2541a3451b277a0a2188d4cb047065372375fb8a
DanielRPTI <drjunior988@hotmail.com>
2023-11-21 07:58:38 -0300

    Teste final de pegar o sumario do User

--- 02-NodeJs-ApiRestful/test/transactions.test.ts
+++ 02-NodeJs-ApiRestful/test/transactions.test.ts
@@ -52,7 +52,7 @@ describe('Transactions routes', () => {
     ])
   })
 
-  it.only('Should be able to list a specific transaction', async () => {
+  it('Should be able to list a specific transaction', async () => {
     const createTransactionResponse = await request(app.server)
       .post('/transactions')
       .send({
@@ -75,10 +75,41 @@ describe('Transactions routes', () => {
       .set('Cookie', cookies)
       .expect(200)
     // verifica se está recebendo um array com objetos
-    expect(getTransactionResponse.body.transaction).toEqual(
+    expect(getTransactionResponse.body.transactions).toEqual(
       expect.objectContaining({
         title: 'New Transaction',
         amount: 5000,
+      }),
+    )
+  })
+  it.only('Should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit Transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get(`/transactions/summary`)
+      .set('Cookie', cookies)
+      .expect(200)
+    // verifica se está recebendo um array com objetos
+    expect(summaryResponse.body.summary).toEqual(
+      expect.objectContaining({
+        amount: 3000,
       }),
     )
   })
